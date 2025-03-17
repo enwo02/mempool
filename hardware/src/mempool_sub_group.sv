9ca62dcf2da4d2829669b777698f568e31a86926
mbertuletti <mbertuletti@iis.ee.ethz.ch>
2025-03-17 09:47:04 +0100

    [hardware] Add RedMulE waves

--- hardware/src/mempool_sub_group.sv
+++ hardware/src/mempool_sub_group.sv
@@ -164,7 +164,7 @@ module mempool_sub_group
     logic              [NumGroups+NumSubGroupsPerGroup-1-1:0] tran_tcdm_slave_resp_valid;
     logic              [NumGroups+NumSubGroupsPerGroup-1-1:0] tran_tcdm_slave_resp_ready;
 
-    if (t < NumRMTilesPerGroup) begin: gen_redmule_tiles
+    if (t < NumRMTilesPerSubGroup) begin: gen_redmule_tile
       mempool_redmule_tile #(
         .TCDMBaseAddr(TCDMBaseAddr),
         .BootAddr    (BootAddr    )
@@ -200,10 +200,10 @@ module mempool_sub_group
         .axi_mst_req_o           (axi_tile_req[t]                                ),
         .axi_mst_resp_i          (axi_tile_resp[t]                               ),
         // Wake up interface
-        .wake_up_i               (wake_up_q[t*NumCoresPerTile])
+        .wake_up_i               (wake_up_q[t])
       );
     end
-    else begin: gen_snitch_tiles
+    else begin: gen_snitch_tile
       mempool_tile #(
         .TCDMBaseAddr(TCDMBaseAddr),
         .BootAddr    (BootAddr    )
@@ -239,7 +239,7 @@ module mempool_sub_group
         .axi_mst_req_o           (axi_tile_req[t]                                ),
         .axi_mst_resp_i          (axi_tile_resp[t]                               ),
         // Wake up interface
-        .wake_up_i               (wake_up_q[t*NumCoresPerTile +: NumCoresPerTile])
+        .wake_up_i               (wake_up_q[((t-NumRMTilesPerSubGroup)*NumCoresPerTile+NumRMTilesPerSubGroup) +: NumCoresPerTile])
       );
     end
 
